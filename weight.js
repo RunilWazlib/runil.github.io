df6b11ceef28fc1eccbaa8c45a013a88d1ce4c13
RunilWazlib <104738652+RunilWazlib@users.noreply.github.com>
2023-02-04 18:57:08 +0530

    new files updated

---
+++ weight.js
@@ -0,0 +1,258 @@
+function mercury() {
+    let a = Number(document.getElementById("weight1").value);
+    let b = (a/(9.8)*3.7);
+  
+    let printWindow = window.open();
+    printWindow.document.write(
+      "<html>" +
+      "<head>" +
+        "<style>" +
+        "body {" +
+        "font-family: Arial, sans-serif;" +
+        "font-weight:bold;"+
+        "background: linear-gradient(to top, rgba(0,0,0,0.6),rgba(0,0,0,0.8)), url(pics/background.jpg);" +
+        "color:#fff;"+
+        " background-repeat: no-repeat;"+
+        "  background-size: cover;"+
+        "   background-attachment: fixed;"+
+      "}" +
+          "#output { font-size: 20px; text-align: center; position:relative; top:50%; }" +
+        "</style>" +
+      "</head>" +
+      "<body>" +
+        "<div id='output'>" +
+          "Weight on Mercury in Kg: " + b +
+        "</div>" +
+      "</body>" +
+      "</html>"
+    );
+  }
+  
+    
+    
+    
+
+
+
+
+function venus()
+{
+    let a=Number(document.getElementById("weight2").value);
+    let b= (a/(9.8)*8.87);
+    let printWindow = window.open();
+    printWindow.document.write(
+      "<html>" +
+      "<head>" +
+        "<style>" +
+        "body {" +
+        "font-family: Arial, sans-serif;" +
+        "font-weight:bold;"+
+        "background: linear-gradient(to top, rgba(0,0,0,0.6),rgba(0,0,0,0.8)), url(pics/background.jpg);" +
+        "color:#fff;"+
+        " background-repeat: no-repeat;"+
+        "  background-size: cover;"+
+        "   background-attachment: fixed;"+
+      "}" +
+          "#output { font-size: 20px; text-align: center; position:relative; top:50%; }" +
+        "</style>" +
+      "</head>" +
+      "<body>" +
+        "<div id='output'>" +
+          "Weight on Venus in Kg: " + b +
+        "</div>" +
+      "</body>" +
+      "</html>"
+  
+    );
+}
+  
+
+function earth()
+{
+    let a=Number(document.getElementById("weight3").value);
+    let b=a*1;
+    let printWindow = window.open();
+    printWindow.document.write(
+        "<html>" +
+        "<head>" +
+          "<style>" +
+          "body {" +
+          "font-family: Arial, sans-serif;" +
+          "font-weight:bold;"+
+          "background: linear-gradient(to top, rgba(0,0,0,0.6),rgba(0,0,0,0.8)), url(pics/background.jpg);" +
+          "color:#fff;"+
+          " background-repeat: no-repeat;"+
+          "  background-size: cover;"+
+          "   background-attachment: fixed;"+
+        "}" +
+            "#output { font-size: 20px; text-align: center; position:relative; top:50%; }" +
+          "</style>" +
+        "</head>" +
+        "<body>" +
+          "<div id='output'>" +
+            "Weight on Earth in Kg: " + b +
+          "</div>" +
+        "</body>" +
+        "</html>"
+    
+      );
+ 
+}
+function mars()
+{
+    let a=Number(document.getElementById("weight4").value);
+    let b=(a/(9.8)*3.711);
+    let printWindow = window.open();
+    printWindow.document.write(
+        "<html>" +
+        "<head>" +
+          "<style>" +
+          "body {" +
+          "font-family: Arial, sans-serif;" +
+          "font-weight:bold;"+
+          "background: linear-gradient(to top, rgba(0,0,0,0.6),rgba(0,0,0,0.8)), url(pics/background.jpg);" +
+          "color:#fff;"+
+          " background-repeat: no-repeat;"+
+          "  background-size: cover;"+
+          "   background-attachment: fixed;"+
+        "}" +
+            "#output { font-size: 20px; text-align: center; position:relative; top:50%; }" +
+          "</style>" +
+        "</head>" +
+        "<body>" +
+          "<div id='output'>" +
+            "Weight on Mars in Kg: " + b +
+          "</div>" +
+        "</body>" +
+        "</html>"
+    
+      );
+   
+}
+
+function jupiter()
+{
+    let a=Number(document.getElementById("weight5").value);
+    let b=(a/(9.8)*24.79);
+    let printWindow = window.open();
+    printWindow.document.write(
+        "<html>" +
+        "<head>" +
+          "<style>" +
+          "body {" +
+          "font-family: Arial, sans-serif;" +
+          "font-weight:bold;"+
+          "background: linear-gradient(to top, rgba(0,0,0,0.6),rgba(0,0,0,0.8)), url(pics/background.jpg);" +
+          "color:#fff;"+
+          " background-repeat: no-repeat;"+
+          "  background-size: cover;"+
+          "   background-attachment: fixed;"+
+        "}" +
+            "#output { font-size: 20px; text-align: center; position:relative; top:50%; }" +
+          "</style>" +
+        "</head>" +
+        "<body>" +
+          "<div id='output'>" +
+            "Weight on Jupiter in Kg: " + b +
+          "</div>" +
+        "</body>" +
+        "</html>"
+    
+      );
+    
+}
+function saturn()
+{
+    let a=Number(document.getElementById("weight6").value);
+    let b=(a/(9.8)*10.44);
+    let printWindow = window.open();
+    printWindow.document.write(
+        "<html>" +
+        "<head>" +
+          "<style>" +
+          "body {" +
+          "font-family: Arial, sans-serif;" +
+          "font-weight:bold;"+
+          "background: linear-gradient(to top, rgba(0,0,0,0.6),rgba(0,0,0,0.8)), url(pics/background.jpg);" +
+          "color:#fff;"+
+          " background-repeat: no-repeat;"+
+          "  background-size: cover;"+
+          "   background-attachment: fixed;"+
+        "}" +
+            "#output { font-size: 20px; text-align: center; position:relative; top:50%; }" +
+          "</style>" +
+        "</head>" +
+        "<body>" +
+          "<div id='output'>" +
+            "Weight on Saturn in Kg: " + b +
+          "</div>" +
+        "</body>" +
+        "</html>"
+    
+      );
+  
+}
+
+function uranus()
+{
+    let a=Number(document.getElementById("weight7").value);
+    let b=(a/(9.8)*8.69);
+    let printWindow = window.open();
+    printWindow.document.write(
+        "<html>" +
+        "<head>" +
+          "<style>" +
+          "body {" +
+          "font-family: Arial, sans-serif;" +
+          "font-weight:bold;"+
+          "background: linear-gradient(to top, rgba(0,0,0,0.6),rgba(0,0,0,0.8)), url(pics/background.jpg);" +
+          "color:#fff;"+
+          " background-repeat: no-repeat;"+
+          "  background-size: cover;"+
+          "   background-attachment: fixed;"+
+        "}" +
+            "#output { font-size: 20px; text-align: center; position:relative; top:50%; }" +
+          "</style>" +
+        "</head>" +
+        "<body>" +
+          "<div id='output'>" +
+            "Weight on Uranus in Kg: " + b +
+          "</div>" +
+        "</body>" +
+        "</html>"
+    
+      );
+   
+}
+
+function neptune()
+{
+    let a=Number(document.getElementById("weight8").value);
+    let b=(a/(9.8)*11.15);
+    let printWindow = window.open();
+    printWindow.document.write(
+        "<html>" +
+        "<head>" +
+          "<style>" +
+          "body {" +
+          "font-family: Arial, sans-serif;" +
+          "font-weight:bold;"+
+          "background: linear-gradient(to top, rgba(0,0,0,0.6),rgba(0,0,0,0.8)), url(pics/background.jpg);" +
+          "color:#fff;"+
+          " background-repeat: no-repeat;"+
+          "  background-size: cover;"+
+          "   background-attachment: fixed;"+
+        "}" +
+            "#output { font-size: 20px; text-align: center; position:relative; top:50%; }" +
+          "</style>" +
+        "</head>" +
+        "<body>" +
+          "<div id='output'>" +
+            "Weight on Neptune in Kg: " + b +
+          "</div>" +
+        "</body>" +
+        "</html>"
+    
+      );
+   
+}
